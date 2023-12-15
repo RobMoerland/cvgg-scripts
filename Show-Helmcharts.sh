1fda60273c1682637a6e001e705a2795f53bca4d
Rob Moerland <rwmoerland@hotmail.com>
2023-12-15 10:42:25 +0100

    modified: Show-Helmcharts.sh

--- Show-Helmcharts.sh
+++ Show-Helmcharts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-helm list -o json | \
+helm list -o json --kube-as-user rivm-cvg-tab $* | \
 jq -r '.[] | [.name, .revision, .chart, .updated] | @tsv  ' | \
 column -t -s$'\t' -N"NAME","REVISION","CHART","UPDATED"
